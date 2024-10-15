b17916163128bae8f8241dbef38ab8d7fbfa36df
richterbalazs <richter.balazsvagyok@gmail.com>
2024-10-15 16:01:57 +0200

    lampakapcsolas javitva

--- lighton/src/App.js
+++ lighton/src/App.js
@@ -5,24 +5,12 @@ import { Jatekter } from './components/Jatekter';
 
 function App() {
   const [lista,setLista] = useState([true, true, false, false, true, true, false, false, true]);
-  const [lepes, setLepes] = useState(0);
 
   function katt(adat){
     const slista = [...lista];
-    if (lepes%2 == 0){
-      slista[adat] = true;
-      console.log(slista[adat]);
-    }else{
-      slista[adat] = false;
-      console.log(slista[adat]);
-    }
+    slista[adat] = !slista[adat];
     setLista([...slista]);
-
-    let sv = lepes;
-    sv++;
-    setLepes(sv);
     
-
 
     // BE KELL ÁLLíTANI HOGY A HÁTTÉRSZÍN ÁLLITÓDJON A LÁMPÁKBAN
     
